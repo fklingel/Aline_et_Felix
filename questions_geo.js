cbdd47ffd8a591df5588b4ab06e73bbbe073b7f3
Felix Klingelhoefer <fel.klingel@gmail.com>
2026-02-16 20:19:54 +0100

    mobile games

--- questions_geo.js
+++ questions_geo.js
@@ -3,19 +3,39 @@
 
 const questions = [
     {
-        image: "images/geo/vacances_ski.jpg", // Exemple
-        text: "Nos premières vacances au ski ensemble. C'était dans les Alpes, mais où exactement ?",
-        coords: [45.9237, 6.8694] // Chamonix (Exemple)
+        image: "images/geo/amsterdam.jpg",
+        text: "",
+        coords: [52.372189, 4.888475]
     },
     {
-        image: "images/geo/plage_ete.jpg",
-        text: "Un coucher de soleil mémorable sur cette plage bretonne.",
-        coords: [48.6500, -2.0167] // Saint-Malo (Exemple)
+        image: "images/geo/vb.jpg",
+        text: "Lieu de notre rencontre et de nos fiançailles.",
+        coords: [46.902294, -0.297276]
     },
     {
-        image: "images/geo/restaurant_paris.jpg",
-        text: "Le restaurant de notre premier anniversaire.",
-        coords: [48.8566, 2.3522] // Paris (Exemple)
+        image: "images/geo/cinq.jpg",
+        text: "",
+        coords: [44.107363, 9.725592]
+    },
+    {
+        image: "images/geo/dubrovnik.jpg",
+        text: "",
+        coords: [42.641293, 18.109321]
+    },
+    {
+        image: "images/geo/islande.jpg",
+        text: "",
+        coords: [64.050179, -16.178718]
+    },
+    {
+        image: "images/geo/cape.jpg",
+        text: "",
+        coords: [-33.906081, 18.421040]
+    },
+    {
+        image: "images/geo/londres.jpg",
+        text: "",
+        coords: [51.507351, -0.127758]
     },
     // Ajoutez vos questions ici. Copiez-collez le bloc {} et modifiez les valeurs.
     // Vous pouvez récupérer les coordonnées sur Google Maps (clic droit > plus d'infos).
